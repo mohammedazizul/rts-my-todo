26f192eb38d4f241642fdd811345f3285af61445
mohammedazizul <mohammedazizulhoquesheikh@gmail.com>
2021-07-27 21:21:59 +0800

    input box added

--- src/components/CreateTodo/CreateTodo.tsx
+++ src/components/CreateTodo/CreateTodo.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const CreateTodo = () => {
+
+    const handleSubmit = () => {
+        alert();
+    }
+
     return (
         <div className="card-create">
             create
+            <div>
+                <label htmlFor="">Topic Name: </label>
+                <input type="text" />
+
+                <p></p>
+                <label htmlFor="details">Details: </label>
+                <textarea name="" id="details" cols={15} rows={5}></textarea>
+                
+                <p></p>
+                <input type="submit" onClick={handleSubmit}/>
+            </div>
         </div>
     );
 };
